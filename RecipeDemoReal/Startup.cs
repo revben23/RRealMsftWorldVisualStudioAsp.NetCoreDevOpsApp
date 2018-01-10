557836e7c3d3116d7c81976b7737593f5c746492
revben23 <1989revben23@users.noreply.github.com>
2018-01-09 19:53:47 -0500

    Go to msftworld.com

--- RecipeDemoReal/Startup.cs
+++ RecipeDemoReal/Startup.cs
@@ -26,6 +26,7 @@ namespace RecipeDemoReal
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
     }
